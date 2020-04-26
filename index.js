7d834a1515223e7ec411e45a77f09570f1122daa
HomsVO <max.vma@yandex.ru>
2020-04-26 19:38:14 +0300

    v1

---
+++ index.js
@@ -0,0 +1,19 @@
+import cfgFile from './cfg.json';
+
+const config = {
+    get : (dotPath) => {
+        const cfg = cfgFile[cfgFile.cfg.env];
+        let pathArray = dotPath.split('.');
+        let currentItem = null;
+        pathArray.forEach((item,index)=>{
+            if(index === 0) currentItem = cfg[item];
+            else currentItem = currentItem[item];
+        })
+        return currentItem;
+    },
+    getAll : ()=>{
+        return cfgFile;
+    }
+}
+
+export default config;
